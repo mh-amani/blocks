3c03aa73c1beee2112029b2a42cfe6bce6cd54ae
Amani Mohammad Hossein <amani@iccluster039.iccluster.epfl.ch>
2024-01-26 10:54:57 +0100

    final training commands

--- run-train.sh
+++ run-train.sh
@@ -28,44 +28,46 @@
 
 # DEVICE=2
 # model.map_location: 'cuda:0'
-BSIZE=256
-DISC='gumbel' # 'gumbel' or 'vqvae' or 'softmax'
+BSIZE=512
+DISC='vqvae' # 'gumbel' or 'vqvae' or 'softmax'
 DEVICE=[0]
 NAME="scan_final"
-LR=0.001
+LR=0.0003
 SEQMODEL='bart' # 'gpt2_gpt2' or 'bart'
-NUM_EPOCHS=2000
-CKPT="'/dlabdata1/masani/blocks/logs/training/runs/scan/curriculum-[0.04, 0.99]-bart-vqvae/2024-01-20_17-14-13/checkpoints/last.ckpt'"
-CKPT="'/dlabdata1/masani/blocks/logs/training/runs/scan/curriculum-[0.01, 0.99]-bart-vqvae/2024-01-22_10-16-48/checkpoints/model-6156-0.4065.ckpt'"
+NUM_EPOCHS=500
 
 # supervised:
 python3 run_train.py +experiment=scan_suponly.yaml datamodule.dataset_parameters.supervision_ratio=[0.01,0.9] model/discretizer=$DISC trainer.devices=$DEVICE datamodule.dataset_parameters.batch_size=$BSIZE +test=True name=$NAME || true
 
 # mixed:
-python3 run_train.py +experiment=scan_mixed.yaml datamodule.dataset_parameters.supervision_ratio=[0.01,0.7] trainer.devices=$DEVICE \
+python3 run_train.py +experiment=scan_mixed.yaml datamodule.dataset_parameters.supervision_ratio=[0.01,0.99] trainer.devices=$DEVICE \
     datamodule.dataset_parameters.batch_size=$BSIZE model/sequence_to_sequence_model=$SEQMODEL model/discretizer=$DISC \
     model.model_params.usex=False model.model_params.usez=False \
-    model.model_params.loss_coeff.zxz=1.0 model.model_params.loss_coeff.xzx=1.0 \
-    model.model_params.loss_coeff.quantization_zxz=0.0 \
+    model.model_params.loss_coeff.zxz=1.0 model.model_params.loss_coeff.xzx=0.0 \
+    model.model_params.loss_coeff.quantization_zxz=0.0 model.model_params.loss_coeff.quantization_xzx=0.0 \
     model.model_params.loss_coeff.supervised_seperated_x=0.0 model.model_params.loss_coeff.supervised_seperated_z=0.0 \
-    callbacks.supervision_scheduler.scheduler_xz.hp_init=0.0 callbacks.supervision_scheduler.scheduler_xz.hp_end=0.0 \
-    callbacks.supervision_scheduler.scheduler_z.hp_init=0.7 callbacks.supervision_scheduler.scheduler_z.hp_end=0.7 \
-    callbacks.supervision_scheduler.scheduler_xz.num_warmup_steps=3 callbacks.supervision_scheduler.scheduler_xz.num_training_steps=100 \
-    callbacks.supervision_scheduler.scheduler_z.num_warmup_steps=3 callbacks.supervision_scheduler.scheduler_z.num_training_steps=100 \
-    +test=True model.optimizer.lr=$LR name=$NAME model.lr_scheduler.monitor="val/loss/zxz" num_epochs=$NUM_EPOCHS logger.wandb.notes="zxz and xzx" logger.wandb.tags=["zxz"]
-
-# continue training:
-python3 run_train.py +experiment=scan_curriculum.yaml datamodule.dataset_parameters.supervision_ratio=[0.01,0.99] trainer.devices=$DEVICE \
-    datamodule.dataset_parameters.batch_size=$BSIZE sequence_to_sequence_model_key=$SEQMODEL discretizer_key=$DISC model.checkpoint_path="$CKPT" \
-    model.substitute_config.model_params.usex=False model.substitute_config.model_params.usez=False \
-    model.substitute_config.model_params.loss_coeff.zxz=1.0 model.substitute_config.model_params.loss_coeff.xzx=0.0 \
-    model.substitute_config.model_params.loss_coeff.quantization_zxz=0.0 \
-    model.substitute_config.model_params.loss_coeff.supervised_seperated_x=0.0 model.substitute_config.model_params.loss_coeff.supervised_seperated_z=0.0 \
     callbacks.supervision_scheduler.scheduler_xz.hp_init=0.0 callbacks.supervision_scheduler.scheduler_xz.hp_end=0.0 \
     callbacks.supervision_scheduler.scheduler_z.hp_init=1.0 callbacks.supervision_scheduler.scheduler_z.hp_end=1.0 \
     callbacks.supervision_scheduler.scheduler_xz.num_warmup_steps=3 callbacks.supervision_scheduler.scheduler_xz.num_training_steps=100 \
     callbacks.supervision_scheduler.scheduler_z.num_warmup_steps=3 callbacks.supervision_scheduler.scheduler_z.num_training_steps=100 \
-    +test=True model.optimizer.lr=$LR name=$NAME model.lr_scheduler.monitor="val/loss/zxz" num_epochs=$NUM_EPOCHS logger.wandb.notes="zxz - continueing" logger.wandb.tags=["zxz"]
+    +test=True model.optimizer.lr=$LR name=$NAME model.lr_scheduler.monitor="val/loss/zxz" num_epochs=$NUM_EPOCHS logger.wandb.notes="zxz" logger.wandb.tags=["zxz"]
+
+# continue training:
+
+CKPT="'/dlabdata1/masani/blocks/logs/training/runs/scan/mixed-[0.04, 0.99]-bart-vqvae/2024-01-17_17-50-19/checkpoints/last.ckpt'"
+# CKPT="'/dlabdata1/masani/blocks/logs/training/runs/scan/mixed-[0.01, 0.99]-bart-gumbel/2024-01-24_15-47-58/checkpoints/model-46116-0.0978.ckpt'"
+
+python3 run_train.py +experiment=scan_curriculum.yaml datamodule.dataset_parameters.supervision_ratio=[0.04,0.99] trainer.devices=$DEVICE \
+    datamodule.dataset_parameters.batch_size=$BSIZE sequence_to_sequence_model_key=$SEQMODEL discretizer_key=$DISC model.checkpoint_path="$CKPT" \
+    model.substitute_config.model_params.usex=False model.substitute_config.model_params.usez=False \
+    model.substitute_config.model_params.loss_coeff.zxz=1.0 model.substitute_config.model_params.loss_coeff.xzx=0.0 \
+    model.substitute_config.model_params.loss_coeff.quantization_zxz=0.0 model.substitute_config.model_params.loss_coeff.quantization_xzx=0.0 \
+    model.substitute_config.model_params.loss_coeff.supervised_seperated_x=1.0 model.substitute_config.model_params.loss_coeff.supervised_seperated_z=1.0 \
+    callbacks.supervision_scheduler.scheduler_xz.hp_init=0.6 callbacks.supervision_scheduler.scheduler_xz.hp_end=0.6 \
+    callbacks.supervision_scheduler.scheduler_z.hp_init=1.0 callbacks.supervision_scheduler.scheduler_z.hp_end=1.0 \
+    callbacks.supervision_scheduler.scheduler_xz.num_warmup_steps=3 callbacks.supervision_scheduler.scheduler_xz.num_training_steps=100 \
+    callbacks.supervision_scheduler.scheduler_z.num_warmup_steps=3 callbacks.supervision_scheduler.scheduler_z.num_training_steps=100 \
+    +test=True model.optimizer.lr=$LR name=$NAME model.lr_scheduler.monitor="val/loss/zxz" num_epochs=$NUM_EPOCHS logger.wandb.notes="mixed  continueing" logger.wandb.tags=["mixed","continue"]
 
 
 # testing
@@ -214,3 +216,5 @@ python3 run_inference.py +experiment/inference=inference datamodule=cfq datamodu
 
 # deactivate
 # module purge
+
+
--- run_scripts/run_unsupsup.sh
+++ run_scripts/run_unsupsup.sh
@@ -4,12 +4,12 @@
 # CKPT="'/dlabdata1/masani/blocks/logs/training/runs/scan/curriculum-[0.01, 0.99]-bart-softmax/2024-01-18_18-30-39/checkpoints/last.ckpt'"
 # DISC='softmax' # 'gumbel' or 'vqvae' or 'softmax'
 
-# # 48tov7q4
-# CKPT="'/dlabdata1/masani/blocks/logs/training/runs/scan/mixed-[0.01, 0.99]-bart-gumbel/2024-01-17_16-26-44/checkpoints/last.ckpt'"
-# DISC='gumbel' # 'gumbel' or 'vqvae' or 'softmax'
+# 48tov7q4
+CKPT="'/dlabdata1/masani/blocks/logs/training/runs/scan/curriculum-[0.01, 0.99]-bart-gumbel/2024-01-25_12-04-48/checkpoints/model-0224-0.0965.ckpt'"
+DISC='gumbel' # 'gumbel' or 'vqvae' or 'softmax'
 
-# m5mxh9pm
-# CKPT="'/dlabdata1/masani/blocks/logs/training/runs/scan/mixed-[0.01, 0.99]-bart-vqvae/2024-01-17_16-39-02/checkpoints/last.ckpt'"
+# # m5mxh9pm
+# CKPT="'/dlabdata1/masani/blocks/logs/training/runs/scan/curriculum-[0.01, 0.99]-bart-vqvae/2024-01-24_10-15-41/checkpoints/model-49184-0.8101.ckpt'"
 # DISC='vqvae' # 'gumbel' or 'vqvae' or 'softmax'
 
 BSIZE=256
@@ -20,6 +20,15 @@ SEQMODEL='bart' # 'gpt2_gpt2' or 'bart'
 NUM_EPOCHS=1000
 
 
+python3 run_train.py +experiment=scan_curriculum.yaml datamodule.dataset_parameters.supervision_ratio=[0.01,0.99] trainer.devices=$DEVICE \
+    datamodule.dataset_parameters.batch_size=$BSIZE sequence_to_sequence_model_key=$SEQMODEL discretizer_key=$DISC model.checkpoint_path="$CKPT" \
+    model.substitute_config.model_params.usex=False model.substitute_config.model_params.usez=False \
+    callbacks.supervision_scheduler.scheduler_xz.hp_init=0.0 callbacks.supervision_scheduler.scheduler_xz.hp_end=0.4 \
+    callbacks.supervision_scheduler.scheduler_z.hp_init=1.0 callbacks.supervision_scheduler.scheduler_z.hp_end=1.0 \
+    callbacks.supervision_scheduler.scheduler_xz.num_warmup_steps=2 callbacks.supervision_scheduler.scheduler_xz.num_training_steps=200 \
+    callbacks.supervision_scheduler.scheduler_z.num_warmup_steps=3 callbacks.supervision_scheduler.scheduler_z.num_training_steps=100 \
+    +test=True model.optimizer.lr=$LR name=$NAME model.lr_scheduler.monitor="val/loss" num_epochs=$NUM_EPOCHS logger.wandb.notes="unsupsup" logger.wandb.tags=["unsupsup"]
+
 python3 run_train.py +experiment=scan_curriculum.yaml datamodule.dataset_parameters.supervision_ratio=[0.02,0.99] trainer.devices=$DEVICE \
     datamodule.dataset_parameters.batch_size=$BSIZE sequence_to_sequence_model_key=$SEQMODEL discretizer_key=$DISC model.checkpoint_path="$CKPT" \
     model.substitute_config.model_params.usex=False model.substitute_config.model_params.usez=False \
@@ -28,4 +37,22 @@ python3 run_train.py +experiment=scan_curriculum.yaml datamodule.dataset_paramet
     callbacks.supervision_scheduler.scheduler_xz.num_warmup_steps=2 callbacks.supervision_scheduler.scheduler_xz.num_training_steps=200 \
     callbacks.supervision_scheduler.scheduler_z.num_warmup_steps=3 callbacks.supervision_scheduler.scheduler_z.num_training_steps=100 \
     +test=True model.optimizer.lr=$LR name=$NAME model.lr_scheduler.monitor="val/loss" num_epochs=$NUM_EPOCHS logger.wandb.notes="unsupsup" logger.wandb.tags=["unsupsup"]
+
+# python3 run_train.py +experiment=scan_curriculum.yaml datamodule.dataset_parameters.supervision_ratio=[0.04,0.99] trainer.devices=$DEVICE \
+#     datamodule.dataset_parameters.batch_size=$BSIZE sequence_to_sequence_model_key=$SEQMODEL discretizer_key=$DISC model.checkpoint_path="$CKPT" \
+#     model.substitute_config.model_params.usex=False model.substitute_config.model_params.usez=False \
+#     callbacks.supervision_scheduler.scheduler_xz.hp_init=0.0 callbacks.supervision_scheduler.scheduler_xz.hp_end=0.4 \
+#     callbacks.supervision_scheduler.scheduler_z.hp_init=1.0 callbacks.supervision_scheduler.scheduler_z.hp_end=1.0 \
+#     callbacks.supervision_scheduler.scheduler_xz.num_warmup_steps=2 callbacks.supervision_scheduler.scheduler_xz.num_training_steps=200 \
+#     callbacks.supervision_scheduler.scheduler_z.num_warmup_steps=3 callbacks.supervision_scheduler.scheduler_z.num_training_steps=100 \
+#     +test=True model.optimizer.lr=$LR name=$NAME model.lr_scheduler.monitor="val/loss" num_epochs=$NUM_EPOCHS logger.wandb.notes="unsupsup" logger.wandb.tags=["unsupsup"]
+
+# python3 run_train.py +experiment=scan_curriculum.yaml datamodule.dataset_parameters.supervision_ratio=[0.08,0.99] trainer.devices=$DEVICE \
+#     datamodule.dataset_parameters.batch_size=$BSIZE sequence_to_sequence_model_key=$SEQMODEL discretizer_key=$DISC model.checkpoint_path="$CKPT" \
+#     model.substitute_config.model_params.usex=False model.substitute_config.model_params.usez=False \
+#     callbacks.supervision_scheduler.scheduler_xz.hp_init=0.0 callbacks.supervision_scheduler.scheduler_xz.hp_end=0.4 \
+#     callbacks.supervision_scheduler.scheduler_z.hp_init=1.0 callbacks.supervision_scheduler.scheduler_z.hp_end=1.0 \
+#     callbacks.supervision_scheduler.scheduler_xz.num_warmup_steps=2 callbacks.supervision_scheduler.scheduler_xz.num_training_steps=200 \
+#     callbacks.supervision_scheduler.scheduler_z.num_warmup_steps=3 callbacks.supervision_scheduler.scheduler_z.num_training_steps=100 \
+#     +test=True model.optimizer.lr=$LR name=$NAME model.lr_scheduler.monitor="val/loss" num_epochs=$NUM_EPOCHS logger.wandb.notes="unsupsup" logger.wandb.tags=["unsupsup"]
 
